7e776e8c6ec24d42fe1d2104811dc4a4439c14d7
sunjiahao <sunjiahao@38f626fb-aae7-46f6-a1cb-11e8a95e7755>
2018-06-08 01:14:31 +0000

    前端首页更新，后台完善

--- code/xyedu/backend/views/category/_form.php
+++ code/xyedu/backend/views/category/_form.php
@@ -17,7 +17,6 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'type')->textInput() ?>
 
     <?= $form->field($model, 'hot')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
